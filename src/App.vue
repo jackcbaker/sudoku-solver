113c5309806d76d4ead238ad52a173b828573eb3
Jack Baker <jbaker12344@gmail.com>
2022-02-17 07:55:55 +0000

    Got styling to a good place

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <div v-for="row in board" :key="row.rowNum">
-    <li v-for="entry in row.rowEntries" :key="entry.coord">
-      Entry {{ entry.coord }} has value {{ entry.value }}
-    </li>
+  <div class="container">
+    <div class="board">
+      <div class="row" v-for="row in board" :key="row.rowNum">
+        <div class="cell" v-for="entry in row.rowEntries" :key="entry.coord">
+          <input class="entry" maxlength="1" size="1" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,10 +17,16 @@ export default {
   },
   data() {
     return {
+      boardSize: 9,
       board: this.initialiseBoardData(9),
       isSolving: false,
       solveButtonText: "Solve",
     };
+  },
+  computed: {
+    cellWidth: function() {
+      return 100 / (this.boardSize) + '%'
+    }
   },
   methods: {
     initialiseBoardRow(boardSize, rowNum) {
@@ -39,3 +49,43 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  div.container {
+    padding-top: 5vmin;
+  }
+
+  div.board {
+    margin: auto;
+    width: 80vmin;
+    height: 80vmin;
+    background-color: chartreuse;
+    padding: 2vmin;
+  }
+
+  div.row {
+    margin: auto;
+    width: 100%;
+    height: v-bind(cellWidth);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+
+  div.cell {
+    width: v-bind(cellWidth);
+    height: 100%;
+    background-color: white;
+    border: 1px solid black;
+  }
+
+  input.entry {
+    width: 80%;
+    height: 80%;
+    margin: 5%;
+    border-color: white;
+    font-size: 250%;
+    text-align: center;
+  }
+</style>
