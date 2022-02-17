9d46a78f65552d6177f0531a9752fd55e01d664e
Baker, Jack <jack.baker@astrazeneca.com>
2022-02-17 09:14:31 +0000

    Added conditional styling

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,18 @@
   <div class="container">
     <div class="board">
       <div class="row" v-for="row in board" :key="row.rowNum">
-        <div class="cell" v-for="entry in row.rowEntries" :key="entry.coord">
-          <input class="entry" maxlength="1" size="1" />
-        </div>
+        <base-cell v-for="entry in row.rowEntries" :key="entry.coord" :cellData=entry :boardSize=boardSize>
+        </base-cell>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import TheBoard from "./components/TheBoard.vue"
+import BaseCell from "./components/BaseCell.vue"
 export default {
   components: {
+    BaseCell
   },
   data() {
     return {
@@ -70,22 +70,5 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-  }
-
-
-  div.cell {
-    width: v-bind(cellWidth);
-    height: 100%;
-    background-color: white;
-    border: 1px solid black;
-  }
-
-  input.entry {
-    width: 80%;
-    height: 80%;
-    margin: 5%;
-    border-color: white;
-    font-size: 250%;
-    text-align: center;
   }
 </style>
---
+++ src/components/BaseCell.vue
@@ -0,0 +1,67 @@
+<template>
+    <div v-bind:class="isInnerSquareEdge">
+        <input class="entry" maxlength="1" size="1" v-model="cellEntry" />
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  props: ['boardSize', 'cellData'],
+  data() {
+    return {
+      cellEntry: ""
+    }
+  },
+  computed: {
+    cellWidth: function() {
+      return 100 / (this.boardSize) + '%'
+    },
+    isInnerSquareEdge: function() {
+      return {
+        'innerSquareBottom': [2, 5, 8].includes(this.cellData.row),
+        'innerSquareTop': [0, 3, 6].includes(this.cellData.row),
+        'innerSquareLeft': [0].includes(this.cellData.col),
+        'innerSquareRight': [2, 5, 8].includes(this.cellData.col),
+      }
+    }
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped>
+  div {
+    width: v-bind(cellWidth);
+    height: 100%;
+    background-color: white;
+    border: 1px solid black;
+  }
+
+  div.innerSquareBottom {
+    border-bottom: 5px solid black;
+  }
+
+  div.innerSquareTop {
+    border-top: 5px solid black;
+  }
+
+  div.innerSquareRight {
+    border-right: 5px solid black;
+  }
+
+  div.innerSquareLeft {
+    border-left: 5px solid black;
+  }
+
+  input.entry {
+    width: 80%;
+    height: 80%;
+    margin: 5%;
+    border-color: white;
+    font-size: 250%;
+    text-align: center;
+  }
+</style>
